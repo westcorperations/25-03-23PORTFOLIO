7568849df2a9c58dd98296a78d28ec178eb2e813
westcorperations <onwusioneejikek04207>
2023-04-02 13:10:46 +0100

    change profile pics

--- src/components/inc/BannerComponent.vue
+++ src/components/inc/BannerComponent.vue
@@ -2,7 +2,7 @@
     <div>
         <section class="grid lg:grid-cols-2 items-center ">
 
-            <div class="md:mt-12 lg:mt-0 mb-12 lg:mb-0 ">
+            <div class="md:mt-12 lg:mt-0 lg:mb-0 ">
                 <div class="block rounded-lg shadow-lg px-6 py-12 md:px-12 lg:-mr-14"
                 style="background: hsla(0, 0%, 100%, 0.55); backdrop-filter: blur(30px);">
                 <div class="">
